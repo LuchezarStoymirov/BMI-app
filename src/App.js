367a6f73a1fb9e7bae88fe2937c6965d63915530
lucho stoimirov <luchostoimirov@gmail.com>
2023-04-09 20:03:30 +0300

    BMI app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+
+  var [bodyInfo, setBodyInfo] = useState({
+    weight: null,
+    height: null
+  })
+
+  var [mouseOver, setMouseOver] = useState(false);
+
+  var MousedOver = (event) => {
+    setMouseOver(true);
+  }
+
+  var MouseLeave = (event) => {
+    setMouseOver(false);
+  }
+
+  var [BMI, setBMI] = useState(null);
+
+
+  var HandleChange = (event) => {
+    var {name, value} = event.target;
+
+    setBodyInfo(prevValue => {
+      return {
+        ...prevValue,
+        [name] : value
+      }
+    })
+  }
+
+  var CalculateBMI = () => {
+    var result = bodyInfo.weight / Math.pow(bodyInfo.height, 2);
+
+    setBMI(Math.round(result));
+  }
+
+
+  return(
+    <div>
+      <div className="Inputs">
+        <div className="weight"><h1>Weight: {bodyInfo.weight}</h1></div>
+        <div className="height"><h1>Height: {bodyInfo.height}</h1></div>
+        <div className="bmi"><h1>BMI: {BMI}</h1></div>
+        <div className="weightinput"><input name="weight" type="number" placeholder="Weight in kilograms" onChange={HandleChange} value={bodyInfo.weight}/></div>
+        <div className="heightinput"><input name="height" type="number" placeholder="Height in meters" onChange={HandleChange} value={bodyInfo.height}/></div>
+        <div className="calcbutton"><button 
+        onClick={CalculateBMI}
+        onMouseEnter={MousedOver}
+        onMouseOut={MouseLeave}
+        style={{backgroundColor: mouseOver? 'black': null, color: mouseOver? 'white': null}}
+        >Calculate BMI</button></div>
+        <h1 className="Itteration">
+          {BMI === 0? '': null}
+          {BMI <= 18 && BMI >= 1? 'You are underweight.' : null}
+          {BMI >= 19 && BMI <= 24? 'You are normal weight.': null}
+          {BMI >= 25? 'You are overweight.' : null}
+        </h1>
+      </div>
     </div>
   );
 }
